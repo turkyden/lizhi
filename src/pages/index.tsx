9077658214c073aebf055f221b515272508b1b19
Dengju Deng <wj871287@gmail.com>
2022-12-17 12:51:39 +0800

    fix: 新增不能失去信仰跳转

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,19 +44,19 @@ export default function IndexPage() {
       <div className="text-xl py-4">Hi 朋友，今日为你推荐</div>
 
       <div className="flex flex-wrap">
-        <div className="mr-6">
+        <Link className="mr-6 mb-8 hover:text-white text-white" to={`/video`}>
           <div className="block text-white w-[410px] h-48 rounded-xl overflow-hidden">
             {/* <video className='w-full' poster='https://www.lizhi334.com/wp-content/uploads/2022/08/lizhi-20-scaled.jpeg' loop muted autoPlay>
               <source src="https://www.lizhi334.com/wp-content/uploads/2022/03/%E9%A6%96%E9%A1%B5.mp4" type='video/mp4' />
             </video> */}
             <img
-              className="object-cover w-full h-48 rounded-xl"
+              className="object-cover w-full h-48 rounded-xl transition transform hover:scale-105 cursor-pointer"
               src="/post/lizhi.jpeg"
               alt="cover"
             />
           </div>
           <div className="pt-4">我们不能失去信仰 · 李志</div>
-        </div>
+        </Link>
         {(window.album as IAlbumList).map((v, i) => (
           <Link
             className="mr-6 mb-8 hover:text-white text-white"
